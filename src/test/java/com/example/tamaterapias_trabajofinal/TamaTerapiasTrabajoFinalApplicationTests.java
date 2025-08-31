4e17bc1791beda02b562bb1d9875387235656d5b
photo <jose.galonso.2010@gmail.com>
2025-08-31 13:55:47 +0200

    Primer Commit, creacion

---
+++ src/test/java/com/example/tamaterapias_trabajofinal/TamaTerapiasTrabajoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tamaterapias_trabajofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamaTerapiasTrabajoFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
