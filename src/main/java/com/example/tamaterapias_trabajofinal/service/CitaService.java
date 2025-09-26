4bc8be7f5fc61c2f375236c462db88f6ce95674e
photo <jose.galonso.2010@gmail.com>
2025-09-26 15:24:27 +0200

    ServicioController ya hecho

---
+++ src/main/java/com/example/tamaterapias_trabajofinal/service/CitaService.java
@@ -0,0 +1,4 @@
+package com.example.tamaterapias_trabajofinal.service;
+
+public class CitaService {
+}
---
+++ src/main/java/com/example/tamaterapias_trabajofinal/service/UsuarioService.java
@@ -0,0 +1,60 @@
+package com.example.tamaterapias_trabajofinal.service;
+
+import com.example.tamaterapias_trabajofinal.modelo.Cita;
+import com.example.tamaterapias_trabajofinal.modelo.Usuario;
+import com.example.tamaterapias_trabajofinal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
+    /**
+     * Función apra crear un usuario
+     * @param usuario
+     * @return
+     */
+    public Usuario crearUsuario(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    /**
+     * Función para eliminar el usuario, si lo pide
+     * @param usuario
+     * @return
+     */
+
+    public void eliminarUsuario(Usuario usuario){
+        usuarioRepository.delete(usuario);
+    }
+
+
+    /**
+     * Función que busca a un usuario por su id, is no está, lo que hace es mandar una excepción
+      * @param id
+     * @return
+     */
+    public Usuario buscarUsuarioID(Integer id){
+        return usuarioRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Usuario no encontrado con el id" + id));
+    }
+
+    /**
+     * Devuelve todos los usuarios que haya
+     * @return
+     */
+    public List<Usuario> listarUsuarios(){
+        return usuarioRepository.findAll();
+    }
+
+
+
+}
